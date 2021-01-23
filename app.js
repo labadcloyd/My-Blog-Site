be052f1513dc486f52b85a62f4e91f4e11cf0b40
cloyd <abadcloyd@gmail.com>
2021-01-23 14:49:33 +0800

    added a schema to the server

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const mongoose = require('mongoose')
 const _ = require('lodash');
 const { kebabCase } = require("lodash");
 
+mongoose.connect('mongodb://localhost:27017/blogSite', {useNewUrlParser:true, useUnifiedTopology: true })
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  date: Date,
+  content: String,
+});
+const article = mongoose.model('article', articleSchema)
+
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
